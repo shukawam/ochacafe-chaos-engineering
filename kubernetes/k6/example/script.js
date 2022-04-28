5d4a8c13ff23365209118d57a6d0ddb57a6c6d7b
shukawam <shukawam@gmail.com>
2022-04-28 14:01:29 +0000

    Fixed

--- kubernetes/k6/example/script.js
+++ kubernetes/k6/example/script.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-    http.get('http://wordpress.shukawam.cf');
+    http.get('http://wordpress.ochacafe-s5.cf');
     sleep(1)
 }
